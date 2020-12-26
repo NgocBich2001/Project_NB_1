15d99c1e600a8a28abdd4183d928683ad94d0ab7
NgocBich2001 <73027931+NgocBich2001@users.noreply.github.com>
2020-12-26 23:06:26 +0700

    ADD

--- Grocery/Grocery/Presenation/FormMenuMain.cs
+++ Grocery/Grocery/Presenation/FormMenuMain.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using Grocery.Utiility;
 using System.Text;
-using System.Security.Cryptography.X509Certificates;
-using Grocery.Presenation;
+using System.Collections.Generic;
 
 namespace Grocery.Presenation
 {
@@ -10,6 +9,111 @@ namespace Grocery.Presenation
     public class FormMenuMain
     {
         
+        public void HienChucNang_TKNhap()
+        {
+            do
+            {
+                Console.Clear();
+                IO.BoxTitle("          **** CÁC CHỨC NĂNG ***", 1, 1, 13, 56);
+                IO.Writexy("   *  1. Thống kê khoản chi theo ngày.    *", 12, 5);
+                IO.Writexy("   *  2. Thống kê khoản chi theo tháng.   *", 12, 6);
+                IO.Writexy("   *  3. Thống kê khoản chi theo năm.     *", 12, 7);
+                IO.Writexy("   *  4. Quay lại.                        *", 12, 8);
+                IO.Writexy("   ****************************************", 12, 9);
+
+                FormThongKe tk = new FormThongKe();
+
+                ConsoleKeyInfo kt = Console.ReadKey();
+                switch (kt.KeyChar)
+                {
+                    case '1':
+                        tk.TK_NgayNhap();
+                        Console.ReadKey();
+                        break;
+                    case '2':
+                        tk.TK_ThangNhap();
+                        Console.ReadKey();
+                        break;
+
+                    case '3':
+                        tk.TK_NamNhap();
+                        Console.ReadKey();
+                        break;
+                    case '4':
+                        HienChucNang_TK();
+                        break;
+                }
+            } while (true);
+        }
+        public void HienChucNang_TKBan()
+        {
+            do
+            {
+                Console.Clear();
+                IO.BoxTitle("          **** CÁC CHỨC NĂNG ***", 1, 1, 13, 56);
+                IO.Writexy("   *  1. Thống kê khoản thu theo ngày.    *", 12, 5);
+                IO.Writexy("   *  2. Thống kê khoản thu theo tháng.   *", 12, 6);
+                IO.Writexy("   *  3. Thống kê khoản thu theo năm.     *", 12, 7);
+                IO.Writexy("   *  4. Quay lại.                        *", 12, 8);
+                IO.Writexy("   ****************************************", 12, 9);
+
+                FormThongKe ftk = new FormThongKe();
+
+                ConsoleKeyInfo kt = Console.ReadKey();
+                switch (kt.KeyChar)
+                {
+                    case '1':
+                        ftk.TK_NgayBan();
+                        Console.ReadKey();
+                        break;
+                    case '2':
+                        ftk.TK_ThangBan();
+                        Console.ReadKey();
+                        break;
+                    case '3':
+                        ftk.TK_NamBan();
+                        Console.ReadKey();
+                        break;
+                    case '4':
+                        HienThi();
+                        break;
+                }
+            } while (true);
+        }
+        public void HienChucNang_TK()
+        {
+            do
+            {
+
+                Console.Clear();
+                IO.BoxTitle("         *** CÁC CHỨC NĂNG ***", 1, 1, 13, 56);
+                IO.Writexy("  *      1. Thống kê khoản thu       *", 12, 5);
+                IO.Writexy("  *      2. Thống kê khoản chi       *", 12, 6);
+                IO.Writexy("  *      3. Thống kê lợi nhuận       *", 12, 7);
+                IO.Writexy("  *      4. Quay lại                 *", 12, 8);
+                IO.Writexy("  ************************************", 12, 9);
+
+                FormThongKe tk = new FormThongKe();
+
+                ConsoleKeyInfo kt = Console.ReadKey();
+                switch (kt.KeyChar)
+                {
+                    case '1':
+                        HienChucNang_TKBan();
+                        break;
+                    case '2':
+                        HienChucNang_TKNhap();
+                        break;
+
+                    //case '3':
+                    //    HienChucNang_TKLoiNhuan();
+                    //    break;
+                    case '4':
+                        HienThi();
+                        break;
+                }
+            } while (true);
+        }
         public static void HienThi()
         {
             Console.BackgroundColor = ConsoleColor.Black;
@@ -18,7 +122,7 @@ namespace Grocery.Presenation
                 
                 Console.Clear();
                 
-                IO.BoxTitle("             *** CÁC CHỨC NĂNG ***         ", 5, 1, 20, 56);
+                IO.BoxTitle("             *** CÁC CHỨC NĂNG ***         ", 5, 1, 22, 56);
                 IO.Writexy("*       1. Hàng hóa.                       *", 12, 5);
                 IO.Writexy("*                                          *",12, 6);
                 IO.Writexy("*       2. Nhà cung cấp.                   *",  12,  7);
@@ -29,16 +133,19 @@ namespace Grocery.Presenation
                 IO.Writexy("*                                          * ", 12,  12);
                 IO.Writexy("*       5. Hóa đơn bán.                    *",  12, 13);
                 IO.Writexy("*                                          * ",  12,  14);
-                IO.Writexy("*       6. Thoát.                          *",  12,  15);
-                IO.Writexy("*                                          *",  12,  16);
-                IO.Writexy("*    Hãy chọn một chức năng để thực hiện!  *",  12,  17);
-                IO.Writexy("********************************************", 12, 19);
+                IO.Writexy("*       6. Thống kê.                       *", 12, 15);
+                IO.Writexy("*                                          * ", 12, 16);
+                IO.Writexy("*       7. Thoát.                          *",  12,  17);
+                IO.Writexy("*                                          *",  12,  18);
+                IO.Writexy("*    Hãy chọn một chức năng để thực hiện!  *",  12,  19);
+                IO.Writexy("********************************************", 12, 20);
 
                 FormHangHoa hanghoa = new FormHangHoa();
                 FormNhanVien nhanvien = new FormNhanVien();
                 FormNhaCC nhacc = new FormNhaCC();
                 FormHoaDonNhap hdn = new FormHoaDonNhap();
                 FormHoaDonBan hdb = new FormHoaDonBan();
+                FormMenuMain fm = new FormMenuMain();
                 ConsoleKeyInfo kt = Console.ReadKey();
                 switch (kt.KeyChar)
                 
@@ -62,6 +169,9 @@ namespace Grocery.Presenation
                         break;
 
                     case '6':
+                        fm.HienChucNang_TK();
+                        break;
+                    case '7':
                         Environment.Exit(0);
                         break;
                 }
---
+++ Grocery/Grocery/Presenation/FormThongKe.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Text;
+using Grocery.Utiility;
+using Grocery.ThucThe;
+using Grocery.Business;
+using Grocery.Business.Interface;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Grocery.Presenation
+{ 
+    class FormThongKe
+    {
+        private string txtfileb = @"E:\GITHUB\Project_NB_1\Grocery\Grocery\Data\HOADONBAN.txt";
+        private string txtfilen = @"E:\GITHUB\Project_NB_1\Grocery\Grocery\Data\HOADONNHAP.txt";
+        public void TK_NgayBan()
+        {
+            string ngay = "";
+            string s;
+            double tongthu = 0;
+
+            Console.Clear();
+            IO.BoxTitle("                                            THỐNG KÊ THEO NGÀY", 1, 1, 10, 112);
+            IO.Writexy("Nhập ngày muốn thống kê:", 5, 4);
+            IO.Writexy("--------------------------------------------------------------------------------------", 3, 7);
+            IO.Writexy("Nhập ngày thống kê định dạng 'yyyy/mm/dd'!", 5, 8);
+
+            do
+            {
+                ngay = Console.ReadLine();
+                if (ngay == "")
+                {
+                    IO.Clear(5,5, 60, ConsoleColor.Black);
+                    IO.Writexy("Nhập lại ngày thống kê...", 5, 5, ConsoleColor.Black, ConsoleColor.White);
+                    IO.Clear(29, 4, 60, ConsoleColor.Black);
+                }
+            } while (ngay == "");
+
+            StreamReader fb = new StreamReader(txtfileb);
+            while ((s = fb.ReadLine()) != null)
+            {
+                String[] tmp = s.Split('\t');
+                if (tmp[2] == ngay)
+                {
+                    double gia = double.Parse(tmp[5]);
+                    int soluong = int.Parse(tmp[4]);
+                    tongthu += gia * soluong;
+                }
+            }
+            IO.Writexy("Khoản thu là:", 5, 6);
+            IO.Writexy(tongthu.ToString(), 18, 6);
+            fb.Close();
+
+            IO.Clear(5, 5, 60, ConsoleColor.Black);
+            IO.Writexy("Nhấn Enter để thoát...", 5, 5);
+            Console.SetCursorPosition(29, 4);
+        }
+        public void TK_ThangBan()
+        {
+            string thang = "";
+            string s;
+            double tongthu = 0;
+
+            Console.Clear();
+            IO.BoxTitle("                                            THỐNG KÊ THEO NGÀY", 1, 1, 10, 112);
+            IO.Writexy("Nhập tháng muốn thống kê:", 5, 4);
+            IO.Writexy("--------------------------------------------------------------------------------------", 3, 7);
+            IO.Writexy("Nhập tháng thống kê định dạng 'yyyy/mm'!", 5, 8);
+
+            do
+            {
+                thang = Console.ReadLine();
+                if (thang == "")
+                {
+                    IO.Clear(5, 5, 60, ConsoleColor.Black);
+                    IO.Writexy("Nhập lại tháng cần thống kê...", 5, 5, ConsoleColor.Black, ConsoleColor.White);
+                    IO.Clear(29, 4, 60, ConsoleColor.Black);
+                }
+            } while (thang == "");
+
+            StreamReader fb = new StreamReader(txtfileb);
+            while ((s = fb.ReadLine()) != null)
+            {
+                String[] tmp = s.Split('\t');
+                String[] d = tmp[2].Split('/');
+                if ((d[0] + "/" + d[1]) == thang)
+                {
+                    int soluong = int.Parse(tmp[4]);
+                    double gia = double.Parse(tmp[5]);               
+                    tongthu += gia * soluong;
+                }
+            }
+            IO.Writexy("Khoản thu là:", 5, 6);
+            IO.Writexy(tongthu.ToString(), 18, 6);
+            fb.Close();
+
+            IO.Clear(5, 5, 60, ConsoleColor.Black);
+            IO.Writexy("Nhấn Enter để thoát...", 5, 5);
+            Console.SetCursorPosition(29, 4);
+        }
+        public void TK_NamBan()
+        {
+            string nam = "";
+            string s;
+            double tongthu = 0;
+
+            Console.Clear();
+            IO.BoxTitle("                                            THỐNG KÊ THEO NGÀY", 1, 1, 10, 112);
+            IO.Writexy("Nhập năm muốn thống kê:", 5, 4);
+            IO.Writexy("--------------------------------------------------------------------------------------", 3, 7);
+            IO.Writexy("Nhập năm thống kê định dạng 'yyyy'!", 5, 8);
+
+            do
+            {
+                nam = Console.ReadLine();
+                if (nam == "" || Convert.ToInt16(nam) <= 0)
+                {
+                    IO.Clear(5, 5, 60, ConsoleColor.Black);
+                    IO.Writexy("Nhập lại năm cần thống kê...", 5, 5, ConsoleColor.Black, ConsoleColor.White);
+                    IO.Clear(29, 4, 60, ConsoleColor.Black);
+                }
+            } while (nam == "" || Convert.ToInt16(nam) <= 0);
+
+            StreamReader fb = new StreamReader(txtfileb);
+            while ((s = fb.ReadLine()) != null)
+            {
+                String[] tmp = s.Split('\t');
+                String[] d = tmp[2].Split('/');
+                if (d[0] == nam)
+                {
+                    int soluong = int.Parse(tmp[4]);
+                    double gia = double.Parse(tmp[5]);
+                    tongthu += gia * soluong;
+                }
+            }
+            IO.Writexy("Khoản thu là:", 5, 6);
+            IO.Writexy(tongthu.ToString(), 18, 6);
+            fb.Close();
+
+            IO.Clear(5, 5, 60, ConsoleColor.Black);
+            IO.Writexy("Nhấn Enter để thoát...", 5, 5);
+            Console.SetCursorPosition(29, 4);
+        }
+        public void TK_NgayNhap()
+        {
+            string ngay = "";
+            string s;
+            double tongchi = 0;
+
+            Console.Clear();
+            IO.BoxTitle("                                        THỐNG KÊ KHOẢN CHI THEO NGÀY", 1, 1, 10, 112);
+            IO.Writexy("Nhập ngày muốn thống kê:", 5, 4);
+            IO.Writexy("--------------------------------------------------------------------------------------", 3, 7);
+            IO.Writexy("Nhập ngày thống kê định dạng 'yyyy/mm/dd'!", 5, 8);
+
+            do
+            {
+                ngay = Console.ReadLine();
+                if (ngay == "")
+                {
+                    IO.Clear(5, 5, 60, ConsoleColor.Black);
+                    IO.Writexy("Nhập lại ngày thống kê...", 5, 5, ConsoleColor.Black, ConsoleColor.White);
+                    IO.Clear(29, 4, 60, ConsoleColor.Black);
+                }
+            } while (ngay == "");
+
+            StreamReader fn = new StreamReader(txtfilen);
+            while ((s = fn.ReadLine()) != null)
+            {
+                String[] tmp = s.Split('\t');
+                if (tmp[4] == ngay)
+                {
+                    int soluong = int.Parse(tmp[6]);
+                    double gia = double.Parse(tmp[7]);                  
+                    tongchi += gia * soluong;
+                }
+            }
+            IO.Writexy("Khoản chi là:", 5, 6);
+            IO.Writexy(tongchi.ToString(), 18, 6);
+            fn.Close();
+
+            IO.Clear(5, 5, 60, ConsoleColor.Black);
+            IO.Writexy("Nhấn Enter để thoát...", 5, 5);
+            Console.SetCursorPosition(29, 4);
+        }
+        public void TK_ThangNhap()
+        {
+            string thang = "";
+            string s;
+            double tongchi = 0;
+
+            Console.Clear();
+            IO.BoxTitle("                                     THỐNG KÊ KHOẢN CHI THEO NGÀY", 1, 1, 10, 112);
+            IO.Writexy("Nhập tháng muốn thống kê:", 5, 4);
+            IO.Writexy("--------------------------------------------------------------------------------------", 3, 7);
+            IO.Writexy("Nhập tháng thống kê định dạng 'yyyy/mm'!", 5, 8);
+
+            do
+            {
+                thang = Console.ReadLine();
+                if (thang == "")
+                {
+                    IO.Clear(5, 5, 60, ConsoleColor.Black);
+                    IO.Writexy("Nhập lại tháng cần thống kê...", 5, 5, ConsoleColor.Black, ConsoleColor.White);
+                    IO.Clear(29, 4, 60, ConsoleColor.Black);
+                }
+            } while (thang == "");
+
+            StreamReader fn = new StreamReader(txtfilen);
+            while ((s = fn.ReadLine()) != null)
+            {
+                String[] tmp = s.Split('\t');
+                String[] d = tmp[4].Split('/');
+                if ((d[0] + "/" + d[1]) == thang)
+                {
+                    int soluong = int.Parse(tmp[6]);
+                    double gia = double.Parse(tmp[7]);
+                    tongchi += gia * soluong;
+                }
+            }
+            IO.Writexy("Khoản thu là:", 5, 6);
+            IO.Writexy(tongchi.ToString(), 18, 6);
+            fn.Close();
+
+            IO.Clear(5, 5, 60, ConsoleColor.Black);
+            IO.Writexy("Nhấn Enter để thoát...", 5, 5);
+            Console.SetCursorPosition(29, 4);
+        }
+        public void TK_NamNhap()
+        {
+            string nam = "";
+            string s;
+            double tongchi = 0;
+
+            Console.Clear();
+            IO.BoxTitle("                                    THỐNG KÊ KHOẢN CHI THEO NĂM", 1, 1, 10, 112);
+            IO.Writexy("Nhập năm muốn thống kê:", 5, 4);
+            IO.Writexy("--------------------------------------------------------------------------------------", 3, 7);
+            IO.Writexy("Nhập năm thống kê định dạng 'yyyy'!", 5, 8);
+
+            do
+            {
+                nam = Console.ReadLine();
+                if (nam == "" || Convert.ToInt16(nam) <= 0)
+                {
+                    IO.Clear(5, 5, 60, ConsoleColor.Black);
+                    IO.Writexy("Nhập lại năm cần thống kê...", 5, 5, ConsoleColor.Black, ConsoleColor.White);
+                    IO.Clear(29, 4, 60, ConsoleColor.Black);
+                }
+            } while (nam == "" || Convert.ToInt16(nam) <= 0);
+
+            StreamReader fn = new StreamReader(txtfilen);
+            while ((s = fn.ReadLine()) != null)
+            {
+                String[] tmp = s.Split('\t');
+                String[] d = tmp[4].Split('/');
+                if (d[0] == nam)
+                {
+                    int soluong = int.Parse(tmp[6]);
+                    double gia = double.Parse(tmp[7]);
+                    tongchi += gia * soluong;
+                }
+            }
+            IO.Writexy("Khoản chi là:", 5, 6);
+            IO.Writexy(tongchi.ToString(), 18, 6);
+            fn.Close();
+
+            IO.Clear(5, 5, 60, ConsoleColor.Black);
+            IO.Writexy("Nhấn Enter để thoát...", 5, 5);
+            Console.SetCursorPosition(29, 4);
+        }
+    }
+}
